45e64ea5824d7fbf1e6f59476b23050f54a16179
bumasi <brn.lstnbrgr@gmail.com>
2021-12-30 21:35:52 +0100

    testfiles

---
+++ runTestfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# todo: loop over all *.txt files of a dir
+./main "testfiles/tc0.txt" "testfiles/tc0.out.txt"
+./main "testfiles/tc1.txt" "testfiles/tc1.out.txt"
+./main "testfiles/tc2.txt" "testfiles/tc2.out.txt"
+./main "testfiles/tc3.txt" "testfiles/tc3.out.txt"
+./main "testfiles/tc4.txt" "testfiles/tc4.out.txt"
+./main "testfiles/tc5.txt" "testfiles/tc5.out.txt"
+./main "testfiles/tc6.txt" "testfiles/tc6.out.txt"
+./main "testfiles/tc7.txt" "testfiles/tc7.out.txt"
+./main "testfiles/tc7s.txt" "testfiles/tc7s.out.txt"
+./main "testfiles/tc8.txt" "testfiles/tc8.out.txt"
+./main "testfiles/tc8s.txt" "testfiles/tc8s.out.txt"
+./main "testfiles/tc_cw2014.txt" "testfiles/tc_cw2014.out.txt"
+./main "testfiles/tc_hr00.txt" "testfiles/tc_hr00.out.txt"
+./main "testfiles/tc_hr01.txt" "testfiles/tc_hr01.out.txt"
+./main "testfiles/tc_reimai.txt" "testfiles/tc_reimai.out.txt"
+# and now the big one
+./main "testfiles/hr_testcase4.txt" "testfiles/hr_testcase4.out.txt"
