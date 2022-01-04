fd8626a42ec663e3fc73ac850c004f82c6269a33
bumasi <brn.lstnbrgr@gmail.com>
2022-01-04 20:57:14 +0100

    Utests adapted

--- ConvexHullUTests.hs
+++ ConvexHullUTests.hs
@@ -18,6 +18,7 @@ import Test.HUnit
 import Data.Ratio ( (%) )
 
 import ConvexHull
+import Utils
 
 {- Points used in the tests -}
 
@@ -258,26 +259,9 @@ hr01L = TestCase $ assertEqual "hr01L" (round (hr01Len * 10)) 83
 
 hackerRankTests = TestLabel "hackerRank" (TestList [hr00,hr01,hr00L,hr01L])
 
-{- file IO -}
-
-io00 = TestCase $ assertEqual "io00" 
-            (stringToIntListList "3\n12 1\n2 2\n-1 5\n") 
-            [[3],[12,1],[2,2],[(-1),5]]
-io01 = TestCase $ assertEqual "io01" 
-            (intListListToPtList [[3],[12,1],[2,2],[(-1),5]]) 
-            [Pt 12 1, Pt 2 2,Pt (-1) 5]
-io02 = TestCase $ assertEqual "io02"
-            (ptToString (Pt 2 (-3))) "2 -3"
-io03 = TestCase $ assertEqual "io03"
-            (ptListToString [Pt 12 1, Pt 2 2,Pt (-1) 5]) 
-            "3\n12 1\n2 2\n-1 5\n"
-
-ioTests = TestLabel "io" (TestList [io00,io01,io02,io03])
-
 {- all tests -}
 
 allTestCases = TestList [
     trigoTests, leftLowestTests, pseudoAngleTests, comparingVectorsTests, 
-    sortingPointsTests, extendHullTests, convexHullTests, hackerRankTests,
-    ioTests]
+    sortingPointsTests, extendHullTests, convexHullTests, hackerRankTests]
                          
